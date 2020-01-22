214312341d4f69bcd432a5a2140b5945716986af
gjocdn <gianjie@ocdigitalnetwork.com>
2020-01-22 10:34:58 +0800

    master

--- components/Layout/BannerInfo.js
+++ components/Layout/BannerInfo.js
@@ -3,13 +3,13 @@ export const  BannerInfo = [{
     "title": "Venture Car Banner 1",
     "captionText": "We are",
     "captionText2": "CaseTrust Accredited",
-    "image": require("../../assets/images/main-banner1.jpg")
+    "image": require("../../assets/images/img1.jpg")
 },
 { 
     "id": 2,
     "title": "Venture Car Banner 2",
     "captionText": "Toyota C-HR Hybrid",
     "captionText2": "Let This Space-Age Design Impress You",
-    "image": require("../../assets/images/main-banner2.jpg")
+    "image": require("../../assets/images/img1.jpg")
 }
 ]

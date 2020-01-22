5570f98c63d72a6125dae64f32e4e49f6c31e3ba
gjocdn <gianjie@ocdigitalnetwork.com>
2020-01-22 11:37:08 +0800

    shift images to static

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,7 @@ import Steps from "Components/Layout/Steps";
 class Index extends Component {
   render() {
     return (
-      <Default>
-        <h1 style={{color:'black'}}>Hello World</h1>
-        <img
-      
-          src={"/static/main-banner.jpg"}
-        />
-
+      <Default>       
         <Banner />
         <BestSellers />
         <Steps />

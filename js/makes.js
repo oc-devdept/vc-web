1f5a101a2b3a999d9bebfee6eb0846d60251f9a2
gjocdn <gianjie@ocdigitalnetwork.com>
2020-01-22 10:48:15 +0800

    master

--- js/makes.js
+++ js/makes.js
@@ -1,5 +1,11 @@
+// export const makes = [
+//   { id: 1, make: "Toyota", image: require("../assets/images/toyota.png") },
+//   { id: 2, make: "Honda", image: require("../assets/images/honda.png") },
+//   { id: 3, make: "Suzuki", image: require("../assets/images/suzuki.png") }
+// ];
+
 export const makes = [
-  { id: 1, make: "Toyota", image: require("../assets/images/toyota.png") },
-  { id: 2, make: "Honda", image: require("../assets/images/honda.png") },
-  { id: 3, make: "Suzuki", image: require("../assets/images/suzuki.png") }
+  { id: 1, make: "Toyota" },
+  { id: 2, make: "Honda" },
+  { id: 3, make: "Suzuki" }
 ];

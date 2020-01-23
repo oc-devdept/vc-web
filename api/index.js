326a4569e8ae416cb77f005eb6d005b02add2dab
gjocdn <gianjie@ocdigitalnetwork.com>
2020-01-23 12:14:57 +0800

    master

--- api/index.js
+++ api/index.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.NODE_ENV === "production"
-         ? "http://159.65.14.175:3001/api"
-         : "http://localhost:3001/api"
+    ? "http://157.230.248.96:3001/api"
+    : "http://localhost:3001/api"
   });
 
 // api.interceptors.request.use(config => {

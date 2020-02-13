908bf867a97851d47c9eee43b5959764e1de6543
gjocdn <gianjie@ocdigitalnetwork.com>
2020-02-13 17:01:28 +0800

    remove missing imports

--- pages/configure/[id].js
+++ pages/configure/[id].js
@@ -2,10 +2,9 @@ import React, { Component } from 'react'
 import Navbar from '../../components/Layout/Navbar';
 import Footer from '../../components/Layout/Footer';
 import { useRouter } from 'next/router';
+
 import Link from 'next/link';
-import {models} from '../../js/models'
-import {makes} from '../../js/makes'
-import {grades} from '../../js/grades'
+
 export default() => {
 const router = useRouter()
 const  pid = router.query.id

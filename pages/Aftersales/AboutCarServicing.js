3bf40671d5452c60de2eaae5784de598f44667ad
Wini30 <63228242+Wini30@users.noreply.github.com>
2020-09-08 18:44:00 +0800

    Aftersales page

---
+++ pages/Aftersales/AboutCarServicing.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+
+import PageBanner from "Components/Shared/PageBanner";
+
+import { Icon } from '@iconify/react';
+import tickIcon from '@iconify/icons-subway/tick';
+
+export default function AboutCarServicing() {
+    return (
+        <div className="aboutCarServicing-area">
+            <PageBanner
+                overlay
+                title="SERVICING DETAILS AND PRICING"
+                caption="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ligula arcu, 
+                tincidunt vitae porttitor vitae, cursus eu dolor. Nullam et felis ipsum. Vestibulum dapibus metus ligula, 
+                vitae porttitor quam consectetur nec."
+                bgImgUrl={"/static/service/serviceBanner.png"}
+            />
+             <div className="header-bar">
+                <h6>CAR SERVICING DETAILS</h6>
+            </div>
+            <div className="section-title without-bg" align="center">
+                <h2>CAR SERVICE PACKAGING TYPES</h2>
+            </div>
+            <div class="row packing-types">
+                <div class="column" align="center">
+                    <img src="/static/service/car_green.png" />
+                    <h6>ASIAN CARS</h6>
+                    <p>
+                        from $85.00*<br />1.5hrs
+                    </p>
+                </div>
+                <div class="column" align="center">
+                    <img src="/static/service/car_purple.png" />
+                    <h6>CONTINENTAL CARS</h6>
+                    <p>
+                        from $135.00*<br />1.5hrs
+                    </p>
+                </div>
+            </div>
+            <hr />
+            <div>
+                <div className="section-title without-bg" align="center">
+                    <h2 className="section-two-header">TYPES OF AVAILABLE CAR SERVICING</h2>
+                </div>
+                <div class="row available-services">
+                    <div class="column">
+                        <p><Icon className="tickIcon" icon={tickIcon} /> Regular Service</p>
+                        <p><Icon className="tickIcon" icon={tickIcon} /> Repairs</p>
+                        <p><Icon className="tickIcon" icon={tickIcon} /> Inspection</p>
+                        <p><Icon className="tickIcon" icon={tickIcon} /> Air-Conditioning</p>
+                        <p><Icon className="tickIcon" icon={tickIcon} /> Battery</p>
+                        <p><Icon className="tickIcon" icon={tickIcon} /> Belts</p>
+                        <p><Icon className="tickIcon" icon={tickIcon} /> Brakes</p>
+                        <p><Icon className="tickIcon" icon={tickIcon} /> Clutch &amp; Transmission</p>
+                        <p><Icon className="tickIcon" icon={tickIcon} /> Electrical System</p>
+                        <p><Icon className="tickIcon" icon={tickIcon} /> Brakes</p>
+                    </div>
+                    <div class="column">
+                        <p><Icon className="tickIcon" icon={tickIcon} /> Exhaust System</p>
+                        <p><Icon className="tickIcon" icon={tickIcon} /> Ignition System</p>
+                        <p><Icon className="tickIcon" icon={tickIcon} /> Lights &amp; Bulbs</p>
+                        <p><Icon className="tickIcon" icon={tickIcon} /> Oil Change</p>
+                        <p><Icon className="tickIcon" icon={tickIcon} /> Sensors</p>
+                        <p><Icon className="tickIcon" icon={tickIcon} /> Steering System</p>
+                        <p><Icon className="tickIcon" icon={tickIcon} /> Suspension System</p>
+                        <p><Icon className="tickIcon" icon={tickIcon} /> Wheel Alignment</p>
+                        <p><Icon className="tickIcon" icon={tickIcon} /> Windows &amp; Windscreen</p>
+                        <p><Icon className="tickIcon" icon={tickIcon} /> Wiper System</p>
+                    </div>
+                </div>
+                <div className="available-text">
+                    <p>*Note: Final servicing fee and time required may vary depending on the type of services performed and brand of engine oil used.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lorem lorem, auctor non nisi et, facilisis accumsan ligula. Praesent euismod velit in dolor aliquet vestibulum. Nulla neque mi, posuere ut malesuada dignissim, tristique sit amet lorem. In eget nisi mollis, aliquam tortor a, egestas diam. Vestibulum mattis hendrerit nulla sit amet dictum. Sed imperdiet neque bibendum sollicitudin fermentum. Quisque ullamcorper nisi vel scelerisque condimentum.</p>
+                    <p>Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer interdum nisi sem, vel pretium ante varius blandit. Aenean dignissim ex ut dui lobortis, id vehicula ante efficitur. Mauris accumsan egestas tempor. Aliquam erat volutpat. Donec pharetra porta vulputate. Nulla facilisi. Vivamus pharetra, eros ut rhoncus ultricies, massa augue volutpat erat, in bibendum nisl est vitae neque. Quisque eu leo auctor massa vehicula faucibus id quis augue.</p>
+                </div>
+            </div>
+        </div>
+    );
+}
--- pages/aftersales.js
+++ pages/aftersales.js
@@ -5,9 +5,14 @@ import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 
-import BookCarServicing from './Aftersales/BookCarServicing'
-function Aftersales() {
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
 
+import BookCarServicing from './Aftersales/BookCarServicing';
+import AboutCarServicing from './Aftersales/AboutCarServicing';
+import TabPanel from './Aftersales/TabPanel'
+
+function Aftersales() {
   const [value, setValue] = React.useState(2);
 
   const handleChange = (event, newValue) => {
@@ -17,21 +22,7 @@ function Aftersales() {
   return (
     <DefaultLayout>
       <section className="aftersales-area">
-        <div className="container">
-          <Paper square>
-            <Tabs
-              value={value}
-              indicatorColor="primary"
-              textColor="primary"
-              onChange={handleChange}
-              aria-label="disabled tabs example"
-            >
-              <Tab label="Book Car Servicing" />
-              <Tab label="About Car Servicing" />
-            </Tabs>
-          </Paper>
-          <BookCarServicing />
-        </div>
+        <TabPanel />
       </section>
     </DefaultLayout>
   );

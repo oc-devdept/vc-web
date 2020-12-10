ea001ef0947fe6d3ce3fde43b1f9e49848968026
JacksonSeowJiaXian <52728758+JacksonSeowJiaXian@users.noreply.github.com>
2020-12-10 12:52:52 +0800

    remove lorem ipsum in servicing details and pricing

--- pages/about-car-servicing.js
+++ pages/about-car-servicing.js
@@ -13,9 +13,6 @@ export default function AboutCarServicing() {
             <PageBanner
                 overlay
                 title="SERVICING DETAILS AND PRICING"
-                caption="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ligula arcu, 
-                tincidunt vitae porttitor vitae, cursus eu dolor. Nullam et felis ipsum. Vestibulum dapibus metus ligula, 
-                vitae porttitor quam consectetur nec."
                 bgImgUrl={"/static/service/serviceBanner.png"}
             />
              <div className="header-bar">
@@ -73,8 +70,6 @@ export default function AboutCarServicing() {
                 </div>
                 <div className="available-text">
                     <p>*Note: Final servicing fee and time required may vary depending on the type of services performed and brand of engine oil used.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lorem lorem, auctor non nisi et, facilisis accumsan ligula. Praesent euismod velit in dolor aliquet vestibulum. Nulla neque mi, posuere ut malesuada dignissim, tristique sit amet lorem. In eget nisi mollis, aliquam tortor a, egestas diam. Vestibulum mattis hendrerit nulla sit amet dictum. Sed imperdiet neque bibendum sollicitudin fermentum. Quisque ullamcorper nisi vel scelerisque condimentum.</p>
-                    <p>Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer interdum nisi sem, vel pretium ante varius blandit. Aenean dignissim ex ut dui lobortis, id vehicula ante efficitur. Mauris accumsan egestas tempor. Aliquam erat volutpat. Donec pharetra porta vulputate. Nulla facilisi. Vivamus pharetra, eros ut rhoncus ultricies, massa augue volutpat erat, in bibendum nisl est vitae neque. Quisque eu leo auctor massa vehicula faucibus id quis augue.</p>
                 </div>
             </div>
         </div>

7a0bf521dc24294244da8042d46e8b7979bccba7
edmund <edmund@ocdigitalnetwork.com>
2021-02-26 17:27:16 +0800

    thank you page for car-servicing

---
+++ pages/thankyou-car-servicing.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import Link from "next/link";
+import Navbar from "Components/Layout/Navbar";
+// import Subscribe from "Components/Common/Subscribe";
+// import Partner from "Components/Common/Partner";
+import Footer from "Components/Layout/Footer";
+
+class Index extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+
+        <div className="thank-you-area">
+          <div className="container">
+            <img src="./static/thank_you.png" />
+            <h1> Your appointment has been successfully registered.</h1>
+            <Link href="/">
+              <a className="btn btn-primary">Go Home</a>
+            </Link>
+          </div>
+        </div>
+
+        {/* <Subscribe />
+        <Partner /> */}
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Index;
